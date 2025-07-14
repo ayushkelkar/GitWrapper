e7baa77da20ea800057bd43c1f4ee7739060d58d
Ayush Kelkar <ayush94klkr@gmail.com>
2025-07-14 21:54:18 +0530

    Final Commit

--- Test/test1.py
+++ Test/test1.py
@@ -2,4 +2,4 @@
 import subprocess
 a = subprocess.run(["git", "--version"], capture_output=True, text=True)
 print("Standard Output: ", a.stdout)
-print("Standard Error: ", a.stderr)
+print("Standard Error : ", a.stderr)
